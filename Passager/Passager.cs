90cb891e37b554611d96e6338efd9a5ba2eb5502
dimokgit <dimokhot@hotmail.com>
2016-01-13 12:14:46 -0500

    Tests added

--- Passager/Passager.cs
+++ Passager/Passager.cs
@@ -7,13 +7,9 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-public class PassageException : Exception {
-  public PassageException(Expression<Func<bool>> test, string message, params object[] parameters)
+public class PassagerException : Exception {
+  public PassagerException(Expression<Func<bool>> test, string message, params object[] parameters)
     : base(string.Format("Validation {{{0}}} failed" + message, TestBody(test), parameters)) {
-    //((new System.Linq.Expressions.Expression.MemberExpressionProxy((new System.Linq.Expressions.Expression.BinaryExpressionProxy((new System.Linq.Expressions.Expression.BinaryExpressionProxy((new System.Linq.Expressions.Expression.LambdaExpressionProxy(test)).Body as System.Linq.Expressions.LogicalBinaryExpression)).Left as System.Linq.Expressions.LogicalBinaryExpression)).Left as System.Linq.Expressions.FieldExpression)).Member).DeclaringType
-    List<MemberInfo> members = new List<MemberInfo>();
-    new Visitor(mi => members.Add(mi)).Visit(test.Body);
-    members.ToArray();
   }
   private static string TestBody(Expression<Func<bool>> test) {
     List<MemberInfo> members = new List<MemberInfo>();
@@ -37,31 +33,30 @@ public class PassageException : Exception {
     }
   }
 }
-public class PassageException<TValue> : Exception {
-  public PassageException(TValue value, Expression<Predicate<TValue>> test, string message, params object[] parameters)
+public class PassagerException<TValue> : Exception {
+  public PassagerException(TValue value, Expression<Predicate<TValue>> test, string message, params object[] parameters)
     : base(string.Format("Value <{0}> didn't pass validation {1}" + message, value, Regex.Split(test + "", "=>")[1].Trim(), parameters)) {
   }
-  public PassageException(Expression<Func<bool>> test, string message, params object[] parameters)
+  public PassagerException(Expression<Func<bool>> test, string message, params object[] parameters)
     : base(string.Format("Validation <{0}> failed" + message, Regex.Split(test + "", "=>")[1].Trim(), parameters)) {
   }
-  public PassageException(Expression<Func<TValue>> getter, TValue value, Expression<Predicate<TValue>> test, string message, params object[] parameters)
+  public PassagerException(Expression<Func<TValue>> getter, TValue value, Expression<Predicate<TValue>> test, string message, params object[] parameters)
     : base(string.Format("Parameter {0}<{1}> didn't pass validation {2}" + message, GetParameterName(getter), value, Regex.Split(test + "", "=>")[1].Trim(), parameters)) {
   }
   private static string GetParameterName(Expression reference) {
     var lambda = reference as LambdaExpression;
     var member = lambda.Body as MemberExpression;
-
     return member.Member.Name;
   }
 }
-public static class Passage {
+public static class Passager {
   public static T ThrowIf<T>(Expression<Func<T>> getter, Expression<Predicate<T>> test) {
     return ThrowIf(getter, test, "");
   }
   public static T ThrowIf<T>(Expression<Func<T>> getter, Expression<Predicate<T>> test, string message, params object[] parameters) {
     var r = getter.Compile()();
     if (test.Compile()(r))
-      throw new PassageException<T>(getter, r, test, message, parameters);
+      throw new PassagerException<T>(getter, r, test, message, parameters);
     return r;
   }
   public static T ThrowIf<T>(this T v, Expression<Predicate<T>> test) {
@@ -70,13 +65,13 @@ public static class Passage {
   public static T ThrowIf<T>(this T v, Expression<Predicate<T>> test, string message = "", params object[] parameters) {
     return ThrowIfImpl(v, test, message, parameters);
   }
-  public static T ThrowIfImpl<T>(T v, Expression<Predicate<T>> test, string message, params object[] parameters) {
+  static T ThrowIfImpl<T>(T v, Expression<Predicate<T>> test, string message, params object[] parameters) {
     if (test.Compile()(v))
-      throw new PassageException<T>(v, test, message, parameters);
+      throw new PassagerException<T>(v, test, message, parameters);
     return v;
   }
   public static void ThrowIf(Expression<Func<bool>> test, string message = "", params object[] parameters) {
     if (test.Compile()())
-      throw new PassageException(test, message, parameters);
+      throw new PassagerException(test, message, parameters);
   }
 }
---
+++ PassagerTests/PassagerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests {
+  [TestClass()]
+  public class PassagerTests {
+    [TestMethod()]
+    [ExpectedException(typeof(PassagerException<int>))]
+    public void ThrowIf() {
+      var i = 0;
+      try {
+        var sw = Stopwatch.StartNew();
+        Assert.AreEqual(0, i.ThrowIf(v => v > 0));
+        Console.WriteLine(new { sw.ElapsedMilliseconds });
+        i.ThrowIf(v => v == 0);
+      } catch (PassagerException<int> exc) {
+        Console.WriteLine(exc.Message);
+        Assert.IsTrue(exc.Message.Contains("v == 0"));
+        throw;
+      }
+    }
+    [TestMethod()]
+    [ExpectedException(typeof(PassagerException<int>))]
+    public void ThrowIfWithReference() {
+      var i = 0;
+      try {
+        var sw = Stopwatch.StartNew();
+        Assert.AreEqual(0, Passager.ThrowIf(() => i, v => v > 0));
+        Console.WriteLine(new { sw.ElapsedMilliseconds });
+        Assert.AreEqual(0, Passager.ThrowIf(() => i, v => v == 0));
+      } catch (PassagerException<int> exc) {
+        Console.WriteLine(exc.Message);
+        Assert.IsTrue(exc.Message.Contains("v == 0"));
+        throw;
+      }
+    }
+    [TestMethod()]
+    [ExpectedException(typeof(PassagerException))]
+    public void ThrowIfNotValid() {
+      var s = "";
+      try {
+        var sw = Stopwatch.StartNew();
+        Passager.ThrowIf(() => s != "");
+        Console.WriteLine(new { sw.ElapsedMilliseconds });
+        sw = Stopwatch.StartNew();
+        Passager.ThrowIf(() => !string.IsNullOrEmpty(s));
+        Console.WriteLine(new { sw.ElapsedMilliseconds });
+        Passager.ThrowIf(() => string.IsNullOrEmpty(s));
+      } catch (PassagerException exc) {
+        Console.WriteLine(exc.Message);
+        Assert.IsTrue(exc.Message.Contains("IsNullOrEmpty"));
+        throw;
+      }
+    }
+  }
+}
